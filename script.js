5c90020dd98c7c084dc7fceeb27688edaa9d5a2e
Thandu Jnana Yasaswini <99401232+Yasaswini25@users.noreply.github.com>
2023-08-10 00:56:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const quoteText = document.querySelector(".quote"),
+authorName = document.querySelector(".author .name"),
+quoteBtn = document.querySelector("button"),
+soundBtn = document.querySelector(".sound"),
+copyBtn = document.querySelector(".copy"),
+instagramBtn = document.querySelector(".insta");
+
+function randomQuote(){
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText="Loading Quote.....";
+fetch("https://api.quotable.io/random").then(res=> res.json()).then(result =>{
+    console.log(result);
+    quoteText.innerText = result.content;
+    authorName.innerText= result.author;
+    quoteBtn.innerText = "New Quote";
+    quoteBtn.classList.remove("loading");
+});
+}
+soundBtn.addEventListener("click",()=>{
+let utterance=new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+speechSynthesis.speak(utterance);
+});
+copyBtn.addEventListener("click",()=>{
+    navigator.clipboard.writeText(quoteText.innerText);
+});
+quoteBtn.addEventListener("click",randomQuote);
